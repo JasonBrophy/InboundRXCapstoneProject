8b676318101bcab7b4843c88c0248d75a0b5db5c
Jason Custodio <jmaxc@Jasons-iMac.local>
2017-01-20 11:11:00 -0800

    About Tab: alert confirmation system created for URL, Phone, Address. BUG: About Description not showing up in iOS Simulator

--- CoreApplicationPrototype/AboutViewController.swift
+++ CoreApplicationPrototype/AboutViewController.swift
@@ -11,9 +11,49 @@ import UIKit
 class AboutViewController: UIViewController {
 
     //Variables
+    
     @IBOutlet weak var UIAboutTextView: UITextView!
+  
+    // Create confirmation alert function
+    func createAlert(title: String, message: String){
+        
+        // Alert style confirmation
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        
+        // Handler used to transition to other code
+        let yesButton = UIAlertAction(title: "YES", style: UIAlertActionStyle.default, handler: nil)  // YES confirm
+        let noButton  = UIAlertAction(title: "NO",  style: UIAlertActionStyle.default, handler: nil)  // NO
+        
+        alert.addAction(yesButton)  // Add YES button to Alert controller
+        alert.addAction(noButton)   // Add NO  button to Alert controller
+        
+        // Completion: do something after alert is displayed
+        present(alert, animated: true, completion: nil)  // Display Alert
+    }
     
+    // Safari Transfer confirmation
+    @IBAction func url() {
+        let inputTitle   = "WEBSITE TRANSFER"
+        let inputMessage = "Do you want to open this website in Safari?"
+        
+        createAlert(title: inputTitle, message: inputMessage)
+    }
     
+    // Map Transfer confirmation
+    @IBAction func address() {
+        let inputTitle   = "MAP TRANSFER"
+        let inputMessage = "Do you want to open this address in Maps?"
+        
+        createAlert(title: inputTitle, message: inputMessage)
+    }
+    
+    // Phone Transfer confirmation
+    @IBAction func phone() {
+        let inputTitle   = "PHONE TRANSFER"
+        let inputMessage = "Do you want to call this number?"
+        
+        createAlert(title: inputTitle, message: inputMessage)
+    }
     
     //Overriding viewDidLayoutSubviews() so when app opens and
     //about and clicked the UIAboutTextView does not start in the middle
@@ -24,7 +64,6 @@ class AboutViewController: UIViewController {
     }
     
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -32,8 +71,7 @@ class AboutViewController: UIViewController {
 
         self.automaticallyAdjustsScrollViewInsets = false
         
-        UIAboutTextView.text = "Paulsen's Pharmacy\n\n"+"Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.\n\n"+"https://www.paulsenspharmacy.com\n\n"+"4246 NE Sandy Blvd \nPortland, OR\n\n"+"(503)287-1163\n\n"+"Monday - Friday 9:00am - 6:30pm\nSaturday 10:00am - 2:00pm"
-        
+        UIAboutTextView.text = "Paulsen's Pharmacy\n\n"+"Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.\n\n"+"Monday - Friday 9:00am - 6:30pm\nSaturday 10:00am - 2:00pm"
     }
 
     override func didReceiveMemoryWarning() {
@@ -41,15 +79,6 @@ class AboutViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+  
 }
