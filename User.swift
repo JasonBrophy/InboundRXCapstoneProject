299dbe9d384f87d54d5762bd760401f148c31a15
Cher Moua <mouacher@pdx.edu>
2017-02-04 15:40:56 -0800

    Added User class with create user, edit user profile, and recovery password with prototype functions

---
+++ User.swift
@@ -0,0 +1,32 @@
+//
+//  User.swift
+//  CoreApplicationPrototype
+//
+//  Created by Cher Moua on 2/4/17.
+//  Copyright © 2017 InboundRXCapstone. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    //email, address, birthday, last name, first name
+    //login status
+    //maybe security question and security answer
+    
+    
+    //account creation function
+    func AccountCreation(email: String, password: String, securityQuestion: String, securityAnswer: String, firstName: String?, lastName: String?, address: String?, birthday: String?){
+        
+        
+    }
+    
+    //edit account information function
+    func EditAccount(email: String?, password: String?, firstName: String?, lastName: String?, address: String?, birthday: String?){
+    }
+    
+    //recover password function
+    func RecoverPassword(){
+    }
+    
+
+}
