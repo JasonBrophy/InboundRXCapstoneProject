5ba9697a6366da84051f0e465863dc491226dd7f
Cher Moua <mouacher@pdx.edu>
2017-02-04 16:30:43 -0800

    add AccountCreation function body

--- User.swift
+++ User.swift
@@ -12,13 +12,28 @@ class User: NSObject {
     //email, address, birthday, last name, first name
     //login status
     //maybe security question and security answer
-    
+
     
     //account creation function
-    func AccountCreation(email: String, password: String, securityQuestion: String, securityAnswer: String, firstName: String?, lastName: String?, address: String?, birthday: String?){
+    func AccountCreation(email: String, password: String, securityQuestion: String, securityAnswer: String, firstName: String, lastName: String, address: String, birthday: String){
+      
+        let defaults = UserDefaults.standard
         
+        if(defaults.object(forKey: email) != nil){
+            //create pop up error
+            //return tuple of (number, string error)
+        }else{
+            
+            let userInformation: [String: String]=["email": email, "password": password, "securityQuestion": securityQuestion, "securityAnswer": securityAnswer, "firstName": firstName, "lastName": lastName, "address":address, "birthday": birthday]
+
+            defaults.setValue(userInformation, forKey: email)
+        }
         
     }
+    
+    
+    
+    
     
     //edit account information function
     func EditAccount(email: String?, password: String?, firstName: String?, lastName: String?, address: String?, birthday: String?){
