8569687c04720656780815375d3aaeb5c2bd4111
lukekwak <lkwak@pdx.edu>
2017-02-14 19:46:59 -0800

    edited the collectionview to show up in the storyboard.

--- CoreApplicationPrototype/RewardsViewController.swift
+++ CoreApplicationPrototype/RewardsViewController.swift
@@ -11,7 +11,10 @@ import UIKit
 class RewardsViewController: UIViewController {
 
     
+    @IBOutlet weak var rewardsCollectionView: UICollectionView!
     @IBOutlet weak var phold: UILabel!
+    //hardcoded array of images.
+    var images = [UIImage(named: "1reward"),UIImage(named: "2reward"),UIImage(named: "3reward"),UIImage(named: "4reward"),UIImage(named: "5reward")]
     var points = 0
     
     func UpdatePoints(){
@@ -19,8 +22,8 @@ class RewardsViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        self.rewardsCollectionView.delegate = self
+        self.rewardsCollectionView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
@@ -28,24 +31,28 @@ class RewardsViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+ 
+
+}
+
+extension RewardsViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //number of kinds of sections. We only want one type of dimension, so return 1
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
-    */
-    
-    
-    
-    
-    
-    
-    
-    
-
+    //count how many images are in the array.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    //fill cell with an image in the array.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = rewardsCollectionView.dequeueReusableCell(withReuseIdentifier: "rewardsCollectionCell", for: indexPath) as! RewardsCollectionViewCell
+        cell.rewardsImageView.image = images[indexPath.row]
+        return cell
+    }
+    //when clicking on a cell, print out which one is getting selected.
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Selected item is: " , indexPath.row)
+    }
 }
---
+++ RewardsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RewardsCollectionViewCell.swift
+//  CoreApplicationPrototype
+//
+//  Created by Luke Kwak on 2/14/17.
+//  Copyright © 2017 InboundRXCapstone. All rights reserved.
+//
+
+import UIKit
+
+class RewardsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var rewardsImageView: UIImageView!
+}
