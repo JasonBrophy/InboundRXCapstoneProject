c909eb6955e8d085a55a5681f5bce8ced1442e09
lukekwak <lkwak@pdx.edu>
2017-02-21 12:31:49 -0800

    collection view now has button and expand cell.

--- RewardsCollectionViewCell.swift
+++ RewardsCollectionViewCell.swift
@@ -8,6 +8,27 @@
 
 import UIKit
 
+protocol RewardsCollectionViewCellDelegate {
+    func redeemDeal(forCell: RewardsCollectionViewCell)
+    func infoDeal(forCell: RewardsCollectionViewCell)
+}
+
 class RewardsCollectionViewCell: UICollectionViewCell {
-    @IBOutlet weak var rewardsImageView: UIImageView!
+    var delegate: RewardsCollectionViewCellDelegate? = nil
+    
+    //background information
+    @IBOutlet weak var infoButton: UIButton!
+    @IBAction func infoButton(_ sender: Any) {
+        delegate?.infoDeal(forCell: self)
+    }
+    
+    //redeem button
+    @IBOutlet weak var redeemButton: UIButton!
+    @IBAction func redeemButton(_ sender: Any) {
+        delegate?.redeemDeal(forCell: self)
+    }
+    //action when the redeem button is pressed.
+    func redeemMe() {
+        redeemButton.setTitle("Redeemed", for: .normal)
+    }
 }
