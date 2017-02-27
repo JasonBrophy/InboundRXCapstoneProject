feb52000689e615f02813fd958a2aa0abdb7f8af
Cher Moua <mouacher@pdx.edu>
2017-02-26 19:09:19 -0800

    Testing out a new possible way to do product pop up

---
+++ CoreApplicationPrototype/PopUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PopUpViewController.swift
+//  CoreApplicationPrototype
+//
+//  Created by Cher Moua on 2/26/17.
+//  Copyright © 2017 InboundRXCapstone. All rights reserved.
+//
+
+import UIKit
+
+class PopUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.black.withAlphaComponent(0.8)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func closePopUp(_ sender: Any) {
+        self.view.removeFromSuperview()
+    }
+
+
+}
