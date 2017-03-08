3172208916ab9fe40bef2df98d8782f24b330a5c
Don Draper <catinahat@Dons-iMac.local>
2017-03-07 18:58:03 -0800

    Merge conflicts

--- CoreApplicationPrototype/HomeViewController.swift
+++ CoreApplicationPrototype/HomeViewController.swift
@@ -40,69 +40,50 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         
         updateHomeUI()
-        // webCallController.printBeaconList()
-//        webCallController.getBeaconList { (beaconList) in
-//            if beaconList != nil {
-//                var i = 0
-//                for dict in beaconList! {
-//                    print("Dictionary \(i):")
-//                    print(dict)
-//                    print("\n---\n")
-//                    i = i+1
-//                }
-//            }
-//        }
-//        
-//        webCallController.getHistoricalEventList { (historicalEventsList) in
-//            if historicalEventsList != nil {
-//                var i = 0
-//                for dict in historicalEventsList! {
-//                    print("Event \(i):")
-//                    print(dict)
-//                    print("\n---\n")
-//                    i = i+1
-//                }
-//            }
-//        }
-//        
-//        webCallController.getRewardsList { (rewardsList) in
-//            if rewardsList != nil {
-//                var i = 0
-//                for dict in rewardsList! {
-//                    print("Reward \(i):")
-//                    print(dict)
-//                    print("\n---\n")
-//                    i = i+1
-//                }
-//            }
-//        }
-//        
-//        webCallController.getDailyDealList { (dailyDealList) in
-//            if dailyDealList != nil {
-//                var i = 0
-//                for dict in dailyDealList! {
-//                    print("Daily deal \(i):")
-//                    print(dict)
-//                    print("\n---\n")
-//                    i = i+1
-//                }
-//            }
-//        }
-
-        // Test alling the new user function
-    //    let newUser = ["email": "newUser2@test.com", "password": "qwerty"]
-        //webCallController.createNewUser(userDict: newUser)
         
-        // Test getting user points
-        // First log a user in, then retrieve and print points
-        webCallController.userLogIn(userDict: ["email": "newuser1@test.com", "password": "123456"])
-        webCallController.getUserPoints { (points) in
-            print("\n\nUser points: "+points!+"\n\n")
+        
+        /*
+        webCallController.printBeaconList()
+        webCallController.getBeaconList { (beaconList) in
+            if beaconList != nil {
+                var i = 0
+                for dict in beaconList! {
+                    print("Dictionary \(i):")
+                    print(dict)
+                    print("\n---\n")
+                    i = i+1
+                }
+            }
+        }
+        
+        
+        webCallController.getHistoricalEventList { (historicalEventsList) in
+            if historicalEventsList != nil {
+                var i = 0
+                for dict in historicalEventsList! {
+                    print("Event \(i):")
+                    print(dict)
+                    print("\n---\n")
+                    i = i+1
+                }
+            }
         }
         webCallController.userLogOut()
         webCallController.getUserPoints { (points) in
             if((points) != nil) {
                 print("\n\nUser points after logging out: "+points!+"\n\n")
+            }
+        }
+        
+        webCallController.getDailyDealList { (dailyDealList) in
+            if dailyDealList != nil {
+                var i = 0
+                for dict in dailyDealList! {
+                    print("Daily deal \(i):")
+                    print(dict)
+                    print("\n---\n")
+                    i = i+1
+                }
             }
         }
  
