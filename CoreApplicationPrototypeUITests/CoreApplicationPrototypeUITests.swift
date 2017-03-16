f488306af8c1aa43070e8150c09fa6de501af07d
Jason Brophy <jbrophy@pdx.edu>
2017-03-16 04:49:27 -0700

    Added tests for UI and unit testing.  UI tests login has been updated, as has settings and tab bar.  Another login test to test a user rushing login has been added to fail for a known error.  Unit testing of user login/logout has been finished as well as for the Daily Deals and Rewards web calls.

--- CoreApplicationPrototypeUITests/CoreApplicationPrototypeUITests.swift
+++ CoreApplicationPrototypeUITests/CoreApplicationPrototypeUITests.swift
@@ -33,68 +33,75 @@ class CoreApplicationPrototypeUITests: XCTestCase {
     //Tests out the app(general): clicking all the tab bars, clicking history tabs, and clicking on deals
     //This is recorded code.
     func testExample() {
-        let app = XCUIApplication()
-        let tabBarsQuery = app.tabBars
-        tabBarsQuery.buttons["Rewards"].tap()
-        tabBarsQuery.buttons["History"].tap()
         
-        let tablesQuery = app.tables
-        tablesQuery.staticTexts["1969"].tap()
-        tablesQuery.staticTexts["1972"].tap()
-        tablesQuery.staticTexts["1984"].tap()
-        
-        let staticText = tablesQuery.staticTexts["1993"]
-        staticText.tap()
-        staticText.tap()
-        tabBarsQuery.buttons["About"].tap()
-        
-        let homeButton = tabBarsQuery.buttons["Home"]
-        homeButton.tap()
-        app.buttons["Daily Deals"].swipeLeft()
-        
-        let xcodemedsImage = app.images["XcodeMeds"]
-        xcodemedsImage.swipeLeft()
-        xcodemedsImage.swipeLeft()
-        xcodemedsImage.swipeLeft()
-        xcodemedsImage.swipeLeft()
-        homeButton.tap()
-        app.images["Paulsens Coffee Bar BW"].tap()
     }
     
     
-    //Tests out the login page.
-    //First, starts at the home page
-    //Click on setting on the home page, then log out
-    //In the next view, sign in page, type in email and password. Make sure password shows by clicking switch
-    //Finally hit login.
-    //NOTE: this test is meant to fail.
+    // Tests out functionality related to a user logging in
+    // These include the restriction to rewards, the settings login/logout and create/edit account buttons status, and whether these entries
+    // update upon moving to logged in or logged out status
     func testLogin(){
-        
         let app = XCUIApplication()
         app.navigationBars["Home"].buttons["Settings"].tap()
         
-        let logOutButton = app.tables.buttons["Log Out"]
-        logOutButton.tap()
-        XCTAssert(app.staticTexts["Log In"].exists)
-        XCTAssertTrue(app.tabBars.buttons["Rewards"].isHittable == false)
+        let tabBarsQuery = app.tabBars
+   
+        // Rewards should pop an error if entering the app without logging in and trying to access it.
+        tabBarsQuery.buttons["Rewards"].tap()
+        XCTAssert(app.alerts["Error"].exists)
+        app.alerts["Error"].buttons["Cancel"].tap()
+        
+        
+        tabBarsQuery.buttons["History"].tap()
+        tabBarsQuery.buttons["Home"].tap()
+        XCTAssert(app.buttons["Log In"].exists)
+        XCTAssert(app.buttons["Create Account"].exists)
+        app.buttons["Log In"].tap()
         let emailTextField = app.textFields["email address"]
         let passwordTextField = app.secureTextFields["password"]
         emailTextField.tap()
-        emailTextField.typeText("bob@example.com")
+        emailTextField.typeText("example@example.com")
         passwordTextField.tap()
-        passwordTextField.typeText("password")
+        passwordTextField.typeText("example")
         app.switches["0"].tap()
         XCTAssert(passwordTextField.exists == false)
         app.switches["1"].tap()
         XCTAssert(passwordTextField.exists)
         app.buttons["Log In"].tap()
-        XCTAssert(app.tabBars.buttons["Rewards"].isHittable)
-
-        
+        XCTAssert(tabBarsQuery.buttons["Home"].exists)
+        tabBarsQuery.buttons["Rewards"].tap()
+        XCTAssert(app.staticTexts["Rewards"].exists)
+        tabBarsQuery.buttons["Home"].tap()
+        app.navigationBars["Home"].buttons["Settings"].tap()
+        XCTAssert(app.buttons["Log Out"].exists)
+        XCTAssert(app.buttons["Edit Account"].exists)
+        app.buttons["Log Out"].tap()
+        XCTAssert(app.buttons["Log In"].exists)
+        tabBarsQuery.buttons["Rewards"].tap()
+        XCTAssert(app.alerts["Error"].exists)
+        app.alerts["Error"].buttons["Cancel"].tap()
     }
     
     
-    //Test out the setting page, So far this is only a switch for notification
+    // Test whether a user rushing to the login page can login
+    // while the login page is pulling from the server
+    func testFastLoginInvalid() {
+        let app = XCUIApplication()
+        app.navigationBars["Home"].buttons["Settings"].tap()
+        app.buttons["Log In"].tap()
+        let emailTextField = app.textFields["email address"]
+        let passwordTextField = app.secureTextFields["password"]
+        emailTextField.tap()
+        emailTextField.typeText("whargarbl")
+        passwordTextField.tap()
+        passwordTextField.typeText("whargarbl")
+        app.buttons["Log In"].tap()
+        XCTAssert(app.alerts["Error"].exists)
+    }
+    
+    
+    //Test out the setting page, So far this is only a switch for notification, and the labels involved.  Login tests the create
+    // and edit button for validity.
     func testSettings(){
         
         let app = XCUIApplication()
@@ -102,31 +109,32 @@ class CoreApplicationPrototypeUITests: XCTestCase {
         settingsButton.tap()
         app.switches["1"].tap()
         XCTAssert(app.switches["0"].exists)
+        XCTAssert(app.staticTexts["Notifications: Off"].exists)
         app.switches["0"].tap()
         XCTAssert(app.switches["1"].exists)
+        XCTAssert(app.staticTexts["Notifications: On"].exists)
+
     }
     
     
     //Tests the tab bar, to make sure the correct view is showing up to the 
-    //corresponding bar button. 
+    //corresponding bar button, needs assertions
     func testTabBar(){
+        XCUIDevice.shared().orientation = .portrait
+        
         let app = XCUIApplication()
         let tabBarsQuery = app.tabBars
-        XCTAssert(app.buttons["Settings"].exists)
-        tabBarsQuery.buttons["Rewards"].tap()
-        XCTAssert(app.staticTexts["Rewards"].exists)
-
-        // XCTAssert(app.buttons["Redeem"].exists)
-        tabBarsQuery.buttons["About"].tap()
-        XCTAssert(app.staticTexts["About"].exists)
-
+        let aboutButton = tabBarsQuery.buttons["About"]
+        aboutButton.tap()
+        
         let historyButton = tabBarsQuery.buttons["History"]
         historyButton.tap()
-        XCTAssert(app.staticTexts["History"].exists)
-        XCTAssert(app.tables.element.exists)
-        let homeButton = tabBarsQuery.buttons["Home"]
-        homeButton.tap()
-        XCTAssert(app.staticTexts["Home"].exists)
+        tabBarsQuery.buttons["Rewards"].tap()
+        app.alerts["Error"].buttons["Cancel"].tap()
+        tabBarsQuery.buttons["Home"].tap()
+        aboutButton.tap()
+        historyButton.tap()
+
     }
     
     
